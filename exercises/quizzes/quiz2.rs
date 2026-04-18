f0836fcbebde3b1fdf2962b6279698468de8a44c
Minhhhh <mineskiroxor@gmail.com>
2026-04-18 14:38:15 +0800

    feat: complete until quiz 2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,20 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+        
+        for (s, c) in input {
+            let transformed = match c {
+                Command::Uppercase => s.to_uppercase(),
+                Command::Append(t) => s + &"bar".repeat(t),
+                Command::Trim => s.trim().to_string()
+            };
+            output.push(transformed);
+        }
+        
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +50,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
