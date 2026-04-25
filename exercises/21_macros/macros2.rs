27757ae45200b13d294bcc273a546b971e3007c7
Minhhhh <mineskiroxor@gmail.com>
2026-04-25 12:39:42 +0800

    feat: complete exe

--- exercises/21_macros/macros2.rs
+++ exercises/21_macros/macros2.rs
@@ -1,10 +1,8 @@
 fn main() {
-    my_macro!();
+    my_macro();
 }
 
 // TODO: Fix the compiler error by moving the whole definition of this macro.
-macro_rules! my_macro {
-    () => {
-        println!("Check out my macro!");
-    };
+fn my_macro () {
+    println!("Check out my macro!");
 }
